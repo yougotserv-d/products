7870bd1cd2359df56bc3a1e9d3adf47c400b3e0a
Alexander Causey <alexander.w.causey@gmail.com>
2021-06-26 11:29:55 -0700

    getProducts, getProduct are complete, getStyles partially so

--- queries.js
+++ queries.js
@@ -3,7 +3,8 @@ const db = require('./database.js');
 const productsQuery = 'SELECT id, name, slogan, description, category, default_price FROM products';
 const productQuery = 'SELECT id, name, slogan, description, category, default_price FROM products WHERE id=$1';
 const featuresQuery = 'SELECT feature, value_name FROM product_features WHERE product_id=$1';
-const stylesQuery = 'SELECT s.id, name, original_price, sale_price, is_default, p.thumbnail_url, p.url, k.id sku, k.size_label, k.quantity FROM styles s LEFT JOIN photos p ON s.id = p.style_id RIGHT JOIN skus k ON s.id = k.style_id WHERE product_id=$1';
+// const stylesQuery = 'SELECT s.id, name, original_price, sale_price, is_default, p.thumbnail_url, p.url, k.id sku, k.size_label, k.quantity FROM styles s LEFT JOIN photos p ON s.id = p.style_id RIGHT JOIN skus k ON s.id = k.style_id WHERE product_id=$1';
+// const stylesQuery = ;
 // const photosQuery = 'SELECT thumbnail_url, url FROM styles s LEFT JOIN photos ON s.id = style_id WHERE product_id=$1';
 // const skusQuery = 'SELECT k.id, k.size_label, k.quantity FROM styles s LEFT JOIN skus k ON s.id = style_id WHERE product_id=$1';
 
@@ -22,7 +23,7 @@ async function getProduct(req, res) {
 
 async function getStyles(req, res) {
   const id = req.params.product_id;
-  const [styles] = await db.multi(stylesQuery, [id]);
+  const styles = await db.query('SELECT row_to_json(a) FROM (SELECT s.id, s.name, s.original_price, s.sale_price, s.is_default, (SELECT array_to_json(array_agg(row_to_json(b))) FROM (SELECT p.thumbnail_url, p.url FROM photos p WHERE p.style_id = s.id) b) AS photos FROM styles s WHERE s.product_id = $1) a', [id]);
   console.log('styles: ', styles);
   res.status(200).send(styles);
 }
